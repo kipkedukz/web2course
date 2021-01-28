14296bf45c37028ea428afc38a7f551a22549ed8
kipkedukz <78065907+kipkedukz@users.noreply.github.com>
2021-01-28 13:00:01 +0600

    Create task.php

---
+++ array/task.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$array = [1,5,7,9];
+//https://www.php.net/manual/ru/array.sorting.php тут о сортировках в  рhp
+//1. отсортируйте массив по возрастанию и присвойте значение в $array_sort
+//$array_sort = тут ваш код
+
+for ($i = 0; $i < count($array); $i++){
+	echo $array[$i];
+    echo "<br>";
+}
+$array_name = ["Федя", "Костя", "Вася"];
+//https://www.php.net/manual/ru/function.array-push.php тут о том, как добавлять элемент к конец массива
+//2. добавьте в $array_name свое имя 
+//тут ваш код
+
+//3. выведите количество элементов в массиве $array_name
+//тут ваш код
+
+//4. если размерность массива $array равна размерности массива $array_name
+//выведите сумму всех элементов массивы $array
+
+//5. иначе выведите разность 
+
+
+
+?>
+
+</body>
+</html>
